f370b07798bc5aa72fb2abe00f7a44c6a54bcd4b
chandra8120 <chandrasekhar8120@gmail.com>
2024-02-14 17:10:35 +0530

    update

--- server/server.js
+++ server/server.js
@@ -21,6 +21,6 @@ mongoose.connect(process.env.MONGO_URL, {
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
