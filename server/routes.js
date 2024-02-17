4b074bf4d5ae766281dd5c3e9c2c46eba955203d
chandra8120 <chandrasekhar8120@gmail.com>
2024-02-17 17:42:33 +0530

    adding new message functionsss

--- server/routes.js
+++ server/routes.js
@@ -12,71 +12,33 @@ router.get("/getvoters", VoterDetailsController.getAllUsers);
 router.delete("/deletevoter/:id", VoterDetailsController.deleteVoter);
 router.get("/getvoter/:id", VoterDetailsController.getVoterById);
 
-router.post('/postreportincident',ReportIncidentController.createReportIncident)
-router.get("/getreportincident",ReportIncidentController.getReportIncident)
-router.post("/getincidentreport", ReportIncidentController.getReportsIncidentByKaryakarthaId);
 
 router.post("/postreportvoter",ReportvoterController.createReportVoter)
 router.get("/getreportvoter",ReportvoterController.getReportController)    
 router.post("/getReportsByKaryakarthaId", ReportvoterController.getReportsByKaryakarthaId);
 router.delete("/deletereportkaryakartha/:id",ReportvoterController.deleteReportById)
-router.post(
-  "/postreportincident",
-  ReportIncidentController.createReportIncident
-);
-router.get("/getreportincident", ReportIncidentController.getReportIncident);
-router.get(
-  "/getincidentreport/:id",
-  ReportIncidentController.getReportsIncidentByKaryakarthaId
-);
 
-router.post(
-  "/postreportincident",
-  ReportIncidentController.createReportIncident
-);
+
+router.post("/postreportincident", ReportIncidentController.createReportIncident);
 router.get("/getreportincident", ReportIncidentController.getReportIncident);
-router.post(
-  "/getincidentreport",
-  ReportIncidentController.getReportsIncidentByKaryakarthaId
-);
+router.post("/getincidentreport",ReportIncidentController.getReportsIncidentByKaryakarthaId);
 
 router.post("/postreportvoter", ReportvoterController.createReportVoter);
 router.get("/getreportvoter", ReportvoterController.getReportController);
-router.post(
-  "/getReportsByKaryakarthaId",
-  ReportvoterController.getReportsByKaryakarthaId
-);
-router.delete(
-  "/deletereportkaryakartha/:id",
-  ReportvoterController.deleteReportById
-);
+router.post("/getReportsByKaryakarthaId",ReportvoterController.getReportsByKaryakarthaId);
+router.delete("/deletereportkaryakartha/:id", ReportvoterController.deleteReportById);
 
 router.post("/postcanvas", CanvasController.createCanvas);
 router.get("/getconvas", CanvasController.getCanvas);
 router.delete("/deletecanvas/:id", CanvasController.deleteCanvasById);
-router.post("/postreportvoter", ReportvoterController.createReportVoter);
-router.get("/getreportvoter", ReportvoterController.getReportController);
-router.get(
-  "/getReportKaryakartha/:id",
-  ReportvoterController.getReportsByKaryakarthaId
-);
-router.delete(
-  "/deletereportkaryakartha/:id",
-  ReportvoterController.deleteReportById
-);
 
-router.post("/postcanvas", CanvasController.createCanvas);
-router.get("/getconvas", CanvasController.getCanvas);
-router.delete("/deletecanvas/:id", CanvasController.deleteCanvasById);
+
 
 router.post("/karyakarthasignup", KaryakarthaLoginController.ksignup);
 router.post("/karyakarthalogin", KaryakarthaLoginController.klogin);
 router.get("/getallkaryakarta", KaryakarthaLoginController.getAll);
 router.put("/verify/:_id", KaryakarthaLoginController.verifyKaryakartha);
 router.post("/createkaryakartha", KaryakarthaLoginController.kSignupVerified);
-router.delete(
-  "/deletekaryakarta/:username",
-  KaryakarthaLoginController.deleteUser
-);
+router.delete("/deletekaryakarta/:username", KaryakarthaLoginController.deleteUser);
 
 export default router;
