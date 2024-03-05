72da8e754248c9d36c35957fe4beff5ba074837d
chandra8120 <chandrasekhar8120@gmail.com>
2024-03-05 19:31:25 +0530

    message phoneN message schema

--- server/Model/messageModel.js
+++ server/Model/messageModel.js
@@ -3,6 +3,7 @@ import {Schema,model} from 'mongoose'
 const messageSchema=new Schema({
     message : {type:String,required:true},
     username:{type:String,required:true},
+    phoneNo:{type:Number,required:true}
     
 })
 const Message=model("Message",messageSchema)
