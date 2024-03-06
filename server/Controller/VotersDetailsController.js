9a27b8937ecee1ae5377f73bcfb1d03b569d150e
chandra8120 <chandrasekhar8120@gmail.com>
2024-03-06 17:00:06 +0530

    voter name update

--- server/Controller/VotersDetailsController.js
+++ server/Controller/VotersDetailsController.js
@@ -27,7 +27,7 @@ const VoterDetailsController = {
     try {
       const { id } = req.params;
       const voter = await Voters.findOne({
-        $or: [{ EPIC_NO: id }, { H_NO: id }, {  MOBILE_NO: id }]
+        $or: [{ EPIC_NO: id }, { H_NO: id }, {  Elector_Name: id }]
       });
   
       if (!voter) {
--- server/server.js
+++ server/server.js
@@ -30,3 +30,5 @@ setupSocket(server);
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
